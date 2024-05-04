fe181a1d81491620a3ed9e8b7268bb666561ce95
SeongKookKIM <ksk8646@naver.com>
2024-05-05 01:09:43 +0900

    Feat/ postData 타입 처리 변경, cookie 타입 변경

--- package/frontend/src/features/Write/container/Write.tsx
+++ package/frontend/src/features/Write/container/Write.tsx
@@ -1,20 +1,28 @@
 import { WriteWrapper } from "../style/Write";
 import cookie from "react-cookies";
-
 import { useLocation } from "react-router-dom";
 import { TEditPostType } from "../../../types/postType";
 import { Section } from "../../../common/styles/Section";
 import AddPostForm from "../../../common/components/Post/container/AddPostForm";
 import PasswordChecked from "../../../common/components/PasswordChecked/container/PasswordChecked";
+import { useEffect, useState } from "react";
 
 function Write() {
   // 쿠키에서 로그인 정보 가져오기
-  const isLogin: string | undefined = cookie.load("isLogin");
+  const isLogin: string | null = cookie.load("isLogin");
+
+  const [postData, setPostData] = useState<TEditPostType | null>(null);
 
   const itemLocation = useLocation();
-  const postData: TEditPostType | null = itemLocation.state
-    ? itemLocation.state.postData
-    : null;
+
+  // postData 타입 오류 처리
+  useEffect(() => {
+    if (itemLocation.state) {
+      setPostData(itemLocation.state.postData);
+    } else {
+      setPostData(null);
+    }
+  }, [itemLocation]);
 
   // 로그인 통과시 Write페이 정상 출력 - 통과 못 할 경우 PasswordCheck
   if (isLogin !== null && isLogin === "pass") {
