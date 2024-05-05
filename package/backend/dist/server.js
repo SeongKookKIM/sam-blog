8045760f4b1a6f209172d6ec9da3a0de45a999c9
SeongKookKIM <ksk8646@naver.com>
2024-05-05 21:35:07 +0900

    vercel test

--- package/backend/dist/server.js
+++ package/backend/dist/server.js
@@ -6,6 +6,15 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const passwordChecked_1 = __importDefault(require("./router/write/passwordChecked"));
+const postTitle_1 = __importDefault(require("./router/write/postTitle"));
+const addPost_1 = __importDefault(require("./router/write/addPost"));
+const addTitle_1 = __importDefault(require("./router/write/addTitle"));
+const subjectCount_1 = __importDefault(require("./router/menu/subjectCount"));
+const postList_1 = __importDefault(require("./router/home/postList"));
+const search_1 = __importDefault(require("./router/search/search"));
+const post_1 = __importDefault(require("./router/post/post"));
+const uploadImage_1 = __importDefault(require("./router/write/uploadImage"));
 const app = (0, express_1.default)();
 app.use(express_1.default.urlencoded({ extended: true }));
 // React Server.js 연결
@@ -18,26 +27,14 @@ app.listen(process.env.PORT || 8080, () => {
   #############################################  
     `);
 });
-// // server-react connect
-// app.use(express.static(path.join(__dirname, "../../frontend/dist")));
-// app.get("/", function (req: Request, res: Response) {
-//   console.log(req);
-//   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-// });
-app.get("/", (_req, res) => {
-    return res.send("Express Typescript on Vercel");
-});
 // Router
-// app.use("/write/passwordChcked", passwordChecked);
-// app.use("/write/postTitle", postTitle);
-// app.use("/write/addPost", addPost);
-// app.use("/write/addTitle", addTitle);
-// app.use("/menu/subjectCount", subjectCount);
-// app.use("/home/postList", postList);
-// app.use("/search", search);
-// app.use("/post", post);
-// app.use("/write/uploadImage", uploadImage);
-// app.get("*", function (req: Request, res: Response) {
-//   res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
-// });
+app.use("/write/passwordChcked", passwordChecked_1.default);
+app.use("/write/postTitle", postTitle_1.default);
+app.use("/write/addPost", addPost_1.default);
+app.use("/write/addTitle", addTitle_1.default);
+app.use("/menu/subjectCount", subjectCount_1.default);
+app.use("/home/postList", postList_1.default);
+app.use("/search", search_1.default);
+app.use("/post", post_1.default);
+app.use("/write/uploadImage", uploadImage_1.default);
 //# sourceMappingURL=server.js.map
