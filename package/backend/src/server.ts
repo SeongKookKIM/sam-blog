45e01ceeb5f675bcb2afc5d98a426cbf23b17b75
SeongKookKIM <ksk8646@naver.com>
2024-05-08 00:54:05 +0900

    port추가

--- package/backend/src/server.ts
+++ package/backend/src/server.ts
@@ -23,7 +23,7 @@ app.use(cors());
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8080, () => {
   console.log(`
   Server listening on port: 8080
   #############################################  
