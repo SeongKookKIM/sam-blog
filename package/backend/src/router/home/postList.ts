28d1d6d38e4008e2415d4da5ba7438ce2a244090
SeongKookKIM <ksk8646@naver.com>
2024-05-08 02:34:18 +0900

    vercel/ test

--- package/backend/src/router/home/postList.ts
+++ package/backend/src/router/home/postList.ts
@@ -6,17 +6,18 @@ postList.use(express.json());
 
 // 메인 주제 추가
 postList.get("/", async (req: Request, res: Response) => {
-  try {
-    const result = await db!.collection("post").find().toArray();
+  return res.send("포스터..");
+  // try {
+  //   const result = await db!.collection("post").find().toArray();
 
-    if (result) {
-      return res.status(200).json(result);
-    } else {
-      return res.status(403).send("PostList 데이터가 없습니다.");
-    }
-  } catch {
-    return res.status(403).send("PostList 데이터를 불러올 수 없습니다.");
-  }
+  //   if (result) {
+  //     return res.status(200).json(result);
+  //   } else {
+  //     return res.status(403).send("PostList 데이터가 없습니다.");
+  //   }
+  // } catch {
+  //   return res.status(403).send("PostList 데이터를 불러올 수 없습니다.");
+  // }
 });
 
 export default postList;
