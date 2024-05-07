b951f6031cafda1b200b2f5d0a0661030156fbca
SeongKookKIM <ksk8646@naver.com>
2024-05-08 02:46:33 +0900

    vercel/ db 흐름 순서 변경

--- package/backend/src/router/home/postList.ts
+++ package/backend/src/router/home/postList.ts
@@ -6,22 +6,17 @@ postList.use(express.json());
 
 // 메인 주제 추가
 postList.get("/", async (req: Request, res: Response) => {
-  if (db!) {
-    return res.send("포스터..");
-  } else {
-    return res.send("db no..");
-  }
-  // try {
-  //   const result = await db!.collection("post").find().toArray();
+  try {
+    const result = await db!.collection("post").find().toArray();
 
-  //   if (result) {
-  //     return res.status(200).json(result);
-  //   } else {
-  //     return res.status(403).send("PostList 데이터가 없습니다.");
-  //   }
-  // } catch {
-  //   return res.status(403).send("PostList 데이터를 불러올 수 없습니다.");
-  // }
+    if (result) {
+      return res.status(200).json(result);
+    } else {
+      return res.status(403).send("PostList 데이터가 없습니다.");
+    }
+  } catch {
+    return res.status(403).send("PostList 데이터를 불러올 수 없습니다.");
+  }
 });
 
 export default postList;
