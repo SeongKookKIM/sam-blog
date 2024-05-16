74db45fa41930c27e6d2e72930249040b1557bda
SeongKookKIM <ksk8646@naver.com>
2024-05-16 20:19:45 +0900

    Fix/ Editor height

--- package/frontend/src/common/components/Post/style/Editor.ts
+++ package/frontend/src/common/components/Post/style/Editor.ts
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const PostContentWrapper = styled.div`
   width: 100%;
   min-height: 500px;
+
+  /* overflow-y: hidden; */
+
   @media (max-width: 900px) {
     min-height: 300px;
   }
